bf10d647c52939e9031457c4815c47c9ac63a7d4
Tiago Gomes <tiagogomes187@gmail.com>
2021-03-16 22:07:47 -0300

    Terminei a Aula - 02

--- paradigma-funcional-java/src/main/java/interfaces_funcionais/Funcoes.java
+++ paradigma-funcional-java/src/main/java/interfaces_funcionais/Funcoes.java
@@ -5,8 +5,8 @@ import java.util.function.Function;
 public class Funcoes {
     public static void main(String[] args) {
         Function<String, String> retornoNomeAoContrario = texto -> new StringBuilder(texto).reverse().toString();
-
-        //PAREI NO TEMPO: 07:00 - FUI DORMIR
+        Function<String, Integer> converterStringParaInteiroCalcularODobro = string -> Integer.valueOf(string) * 2;
         System.out.println(retornoNomeAoContrario.apply("Tiago"));
+        System.out.println(converterStringParaInteiroCalcularODobro.apply("20"));
     }
 }
