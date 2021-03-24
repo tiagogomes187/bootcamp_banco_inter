3c06e50acc37028a5f11c6f71723d8d1b2d1281b
Tiago Gomes <tiagogomes187@gmail.com>
2021-03-24 02:18:10 -0300

    Parei no Time 12:00 - Introdução ao Framework spring boot.

---
+++ Praticando programação em Java/desafio/src/digital/innovation/one/desafio01/MinhaPrimeiraClasse.java
@@ -0,0 +1,16 @@
+package digital.innovation.one.desafio01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class MinhaPrimeiraClasse {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+
+    }
+}
